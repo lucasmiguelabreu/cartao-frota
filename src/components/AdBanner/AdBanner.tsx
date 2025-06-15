3ffed9b8c175685e503b64542bf9e1ca88743f6e
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2025-06-15 18:06:53 -0300

    configuração do tamanho do anuncio

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 import { X } from "lucide-react";
 
 interface AdBannerProps {
+  /** URLs das imagens para exibir no carrossel */
   images: string[];
+  /** Posição fixa na tela */
   position?: "bottom-left" | "bottom-right";
 }
 
@@ -16,6 +18,7 @@ export const AdBanner: FC<AdBannerProps> = ({
   const [visible, setVisible] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // Exibe sempre que houver imagens
   useEffect(() => {
     if (images.length) setVisible(true);
   }, [images]);
@@ -29,16 +32,17 @@ export const AdBanner: FC<AdBannerProps> = ({
     <div
       className={`
         fixed ${posClass} bottom-4
-        w-64               /* mobile: 16rem */
-        sm:w-72            /* ≥640px: 18rem */
-        md:w-80            /* ≥768px: 20rem */
-        lg:w-96            /* ≥1024px:24rem */
-        xl:w-[30rem]       /* ≥1280px:30rem */
+        w-[85vw]            /* Mobile: 85% viewport width */
+        sm:w-[70vw]         /* ≥640px: 70% viewport width */
+        md:w-[50vw]         /* ≥768px: 50% viewport width */
+        lg:w-[40vw]         /* ≥1024px: 40% viewport width */
+        xl:w-[30vw]         /* ≥1280px: 30% viewport width */
+        2xl:w-[25vw]        /* ≥1536px: 25% viewport width */
         overflow-hidden
         z-50
       `}
     >
-      {/* Fechar */}
+      {/* Botão fechar junto à borda */}
       <button
         onClick={handleClose}
         className="absolute top-1 right-1 p-1 bg-white/70 rounded-full hover:bg-white transition z-10"
@@ -47,8 +51,18 @@ export const AdBanner: FC<AdBannerProps> = ({
         <X size={20} className="text-gray-800" />
       </button>
 
-      {/* Conteúdo do banner */}
-      <div className="relative w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-80">
+      {/* Imagem responsiva, altura proporcional */}
+      <div
+        className={`
+          relative w-full
+          h-[60vw]           /* Mobile: 60% viewport height */
+          sm:h-[50vw]        /* ≥640px: 50% vh */
+          md:h-[35vw]        /* ≥768px: 35% vh */
+          lg:h-[30vw]        /* ≥1024px:30% vh */
+          xl:h-[25vw]        /* ≥1280px:25% vh */
+          2xl:h-[20vw]       /* ≥1536px:20% vh */
+        `}
+      >
         <Image
           src={images[currentIndex]}
           alt={`Anúncio ${currentIndex + 1}`}
